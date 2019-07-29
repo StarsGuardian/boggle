fe07af381d7c18a74945cc6115be1fb531db2cc4
Jian <jianfang@email.arizona.edu>
2019-07-29 12:48:58 -0700

    Ready

--- model/DiceTray.java
+++ model/DiceTray.java
@@ -1,28 +1,92 @@
 package model;
 
+/**
+ * 
+ * @author Jian Fang
+ *
+ */
 public class DiceTray {
+	/**
+	 * Construct a tray of dice using a hard coded 2D array of chars. Use this for
+	 * testing
+	 * 
+	 * @param newBoard The 2D array of characters used in testing
+	 */
 
-  /**
-   * Construct a tray of dice using a hard coded 2D array of chars. Use this for
-   * testing
-   * 
-   * @param newBoard
-   *          The 2D array of characters used in testing
-   */
-  public DiceTray(char[][] newBoard) {
-   // TODO Implement this constructor
-  }
+	private char[][] Board;
+	private int[][] used;
 
-  /**
-   * Return true if search is word that can found on the board following the rules
-   * of Boggle
-   * 
-   * @param str
-   *          A word that may be in the board by connecting consecutive letters
-   * @return True if search is found
-   */
-  public boolean found(String attempt) {
-    // TODO: Implement this method
-    return ! true;
-  }
+	public DiceTray(char[][] newBoard) {
+		// TODO Implement this constructor
+
+		used = new int[newBoard.length][newBoard[0].length];
+		Board = newBoard;
+		setZeros();
+	}
+
+	/**
+	 * Return true if search is word that can found on the board following the rules
+	 * of Boggle
+	 * 
+	 * @param str A word that may be in the board by connecting consecutive letters
+	 * @return True if search is found
+	 */
+	public boolean found(String attempt) {
+		// TODO: Implement this method
+		attempt = attempt.toUpperCase();
+		int i = 0;
+		int j = 0;
+		if (attempt.length() == 17) {
+			int idx = attempt.indexOf("QU");
+			if (idx == -1)
+				return false;
+			String s = new String();
+			for (i = 0; i < attempt.length(); i++)
+				if (attempt.charAt(i) != 'U')
+					s += attempt.charAt(i);
+			attempt = s;
+		}
+
+		if (attempt.length() < 3 || attempt.length() > 16)
+			return false;
+
+		for (i = 0; i < Board.length; i++) {
+			setZeros();
+			for (j = 0; j < Board[0].length; j++) {
+				if (FindNext(attempt, 0, i, j))
+					return true;
+			}
+		}
+		return false;
+	}
+
+	private void setZeros() {
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < Board.length; i++) {
+			for (j = 0; j < Board[0].length; j++) {
+				used[i][j] = 0;
+			}
+		}
+	}
+
+	private boolean FindNext(String attempt, int start, int r, int c) {
+		if (r < 0 || r >= Board.length || c < 0 || c >= Board[0].length)
+			return false;
+		if (start == attempt.length())
+			return true;
+		if (attempt.charAt(start) != Board[r][c] || used[r][c] == 1)
+			return false;
+
+		used[r][c] = 1;
+		if (FindNext(attempt, start + 1, r + 1, c) || FindNext(attempt, start + 1, r - 1, c)
+				|| FindNext(attempt, start + 1, r, c + 1) || FindNext(attempt, start + 1, r, c - 1)
+				|| FindNext(attempt, start + 1, r + 1, c + 1) || FindNext(attempt, start + 1, r + 1, c - 1)
+				|| FindNext(attempt, start + 1, r - 1, c + 1) || FindNext(attempt, start + 1, r - 1, c - 1)) {
+			return true;
+		}
+
+		used[r][c] = 0;
+		return false;
+	}
 }
